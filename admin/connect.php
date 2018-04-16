20653dc6afe1f6a2b455ffd6a25497dcde7de2ef
Alemarnery <aleemar.95@gmail.com>
2018-04-15 21:00:26 -0400

    iniciando

--- admin/connect.php
+++ admin/connect.php
@@ -1,10 +1,10 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', '', 'jower');
+$connection = mysqli_connect('localhost', 'root', '', 'funeraria');
   
 if (!$connection) {
     die("Database Connection Failed" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db($connection, 'jower');
+$select_db = mysqli_select_db($connection, 'funeraria');
 if (!$select_db) {
     die("Database Selection Failed" . mysqli_error($connection));
 }
--- funciones/templates/header.php
+++ funciones/templates/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
-	<title>Document</title>
+	<title>Funeraria</title>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" href="css/materialize.css">
 	<link rel="stylesheet" href="css/toastr.css">
@@ -19,18 +19,18 @@
                           <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                           <ul class="right hide-on-med-and-down">
                             <li><a href="admin/index.php">Entrar al Sistema</a></li>
-                            <li><a href="badges.html">Components</a></li>
-                            <li><a href="collapsible.html">Javascript</a></li>
-                            <li><a href="mobile.html">Mobile</a></li>
+                            <li><a href="#">Components</a></li>
+                            <li><a href="#">Javascript</a></li>
+                            <li><a href="#">Mobile</a></li>
                           </ul>
                         </div>
                       </nav>
 
                           <ul class="sidenav" id="mobile-demo">
-                            <li><a href="sass.html">Sass</a></li>
-                            <li><a href="badges.html">Components</a></li>
-                            <li><a href="collapsible.html">Javascript</a></li>
-                            <li><a href="mobile.html">Mobile</a></li>
+                          <li><a href="admin/index.php">Entrar al Sistema</a></li>
+                            <li><a href="#">Components</a></li>
+                            <li><a href="#">Javascript</a></li>
+                            <li><a href="#">Mobile</a></li>
                           </ul>         
            </div>
 	</header>
