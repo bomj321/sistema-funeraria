e82673461007c58b9d0ef9237019d24efdbbd027
Jose Ortega <mauricio1314@hotmail.com>
2018-04-23 13:58:28 -0400

    Arreglando el registro

--- admin/registro_action.php
+++ admin/registro_action.php
@@ -16,23 +16,37 @@ $sql="SELECT usuario,pass FROM usuario_admin WHERE usuario= ? ";
 $resultado=mysqli_prepare($connection, $sql);
   mysqli_stmt_bind_param($resultado, "s", $usuario);    
   mysqli_stmt_execute($resultado);
-  mysqli_stmt_bind_result($resultado, $usuario2, $pass_cifrado);
+  mysqli_stmt_store_result($resultado);
+  $fila= mysqli_stmt_num_rows($resultado);
 
-        while (mysqli_stmt_fetch($resultado)) {                 
-                 if (password_verify($password, $pass_cifrado)) {
+        if ($fila ==0){
+                               echo "
+                                 <script>
+                                        alert('Usuario Incorrecto');
+                                        window.location.href ='index.php';
+                                 </script>  
+                            ";
+                       }
+
+  mysqli_stmt_bind_result($resultado, $usuario2, $pass_cifrado);
+        while (mysqli_stmt_fetch($resultado)) {  
+
+              if (password_verify($password, $pass_cifrado)) {
                             echo "
                                  <script>
                                         alert('Registro Completo');
                                         window.location.href ='sistema.php';
                                  </script>  
                             ";
-                } else {
+
+                }else {
                                  echo "
                                  <script>
-                                        alert('Usuario Incorrecto');
+                                        alert('Contraseña Incorrecta');
                                         window.location.href ='index.php';
                                  </script>  
                             ";
+                            
                        }
         }
 
